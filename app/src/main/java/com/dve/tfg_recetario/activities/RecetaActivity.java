9778948c740723f467d358f06148fe3c79c43037
dvaquerizoenc <davidvaquerizoenc@gmail.com>
2026-03-02 12:33:14 +0100

    Update RecetaActivity.java

--- app/src/main/java/com/dve/tfg_recetario/activities/RecetaActivity.java
+++ app/src/main/java/com/dve/tfg_recetario/activities/RecetaActivity.java
@@ -18,6 +18,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
@@ -83,6 +87,13 @@ public class RecetaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receta);
+
+        View root = findViewById(R.id.main);
+        ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         loadDialog(2000);
 
